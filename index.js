a01cfdeb6a65ffb6878db29f59ce157fe67398d6
Yasser Benseghier <93477835+Yasser4Coder@users.noreply.github.com>
2025-03-09 00:56:49 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+import cors from "cors";
+
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+const DB_URL = process.env.MONGODB_URL;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("API insurence");
+});
+
+mongoose
+  .connect(DB_URL)
+  .then(() => console.log("Database connected"))
+  .catch((err) => console.log(err && "Database connection failed"));
+
+app.listen(PORT, () => {
+  console.log("the server is running");
+});
