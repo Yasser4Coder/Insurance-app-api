1263ee2ac78ee2e785557928a2028075f06a1dd1
Yasser Benseghier <93477835+Yasser4Coder@users.noreply.github.com>
2025-03-09 01:28:25 +0100

    test

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("API insurence");
+  res.status(200).json({ Message: "Insurence API" });
 });
 
 mongoose
