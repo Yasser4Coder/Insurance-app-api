46d0122b245b8e129ea997559ab8baf0f31dfee7
Yasser Benseghier <93477835+Yasser4Coder@users.noreply.github.com>
2025-04-12 14:32:02 +0100

    models

--- index.js
+++ index.js
@@ -12,7 +12,13 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 app.get("/", (req, res) => {
   res.status(201).json({ Message: "Insurence API" });
---
+++ src/models/Vehicle.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const VehicleSchema = new mongoose.Schema({
+  owner: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  registrationNumber: String,
+  model: String,
+  brand: String,
+  year: Number,
+  chassisNumber: String,
+});
+
+const Vehicle = mongoose.model("Vehicle", VehicleSchema);
+export default Vehicle;
